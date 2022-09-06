06d1ee231b16f011ef075121af9459f436cb2c81
Pilot1782 <carson.bountry@gmail.com>
2022-09-06 16:15:36 +0000

    slight fixes

--- setup.py
+++ setup.py
@@ -27,23 +27,10 @@ fncs.dprint(inp)
 path = inp
 
 def imports():
-  try:
-    #x = subprocess.check_output("python3 --version",shell=True)
-    x = subprocess.check_output("python3.10 --version",shell=True)
-  except subprocess.CalledProcessError as err:
-    #fncs.log(err)
-    pass
-  try:
-    x = x.split(" ")
-    y = x[1].split(".")
-    if int(y[1]) >= 6:
-      os.system("python3 -m pip install -r requirements.txt")
-  except Exception as err:
-    str(err)
-    os.system("python -m pip install -r requirements.txt")
-  finally:
-    with open(f"{path}log.txt","w") as fp:
-      fp.write(f"[{fncs.ptime()}] Finished Install Packages and created setup_done.yay file.\n")
+  os.system("python -m pip install -r requirements.txt")
+  
+  with open(f"{path}log.txt","w") as fp:
+    fp.write(f"[{fncs.ptime()}] Finished Install Packages and created setup_done.yay file.\n")
 
 def replace_line(file_name, line_num, text): # Yes i know this is a dumb way to solve it but it works
     lines = open(file_name, 'r').readlines()
@@ -68,7 +55,6 @@ def fix_files():
     with open("setup_done.yay","w") as file:
       pass
 
-
   print("Updating file paths...")
   inp = r"{}".format(inp)
   replace_line(f"{inp}.env",0,r'{}PATH={}settings.json{}'.format("\r",inp,"\n"))
@@ -76,7 +62,7 @@ def fix_files():
   replace_line(f"{inp}settings.json",7,r'  "home-dir": "{}",{}'.format(inp,"\n"))
 
 def verify():
-  print("Please verify the following information is correct")
+  print("Please verify the following information is correct\n")
   print(printfl(inp+".env"),end+"\n==================================\n")
   print(printfl(inp+"settings.json"))
 
