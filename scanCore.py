8bc58544eda1c6f70654413982724fea641e4090
Pilot1782 <34791564a@gmail.com>
2023-03-13 07:54:42 -0600

    sync

--- scanCore.py
+++ scanCore.py
@@ -10,13 +10,14 @@ import asyncio
 import funcs
 import sys
 
-useWebHook, pingsPerSec , maxActive = False, 4800, 10
-masscan_search_path = ('masscan',
-                       '/usr/bin/masscan',
-                       '/usr/local/bin/masscan',
-                       '/sw/bin/masscan',
-                       '/opt/local/bin/masscan'
-                       )
+useWebHook, pingsPerSec, maxActive = False, 4800, 10
+masscan_search_path = (
+    "masscan",
+    "/usr/bin/masscan",
+    "/usr/local/bin/masscan",
+    "/sw/bin/masscan",
+    "/opt/local/bin/masscan",
+)
 try:
     from privVars import *
 except ImportError:
@@ -52,13 +53,15 @@ fncs = funcs.funcs(collection=col)
 
 
 def print(*args, **kwargs):
-    fncs.print(' '.join(map(str, args)), **kwargs)
+    fncs.print(" ".join(map(str, args)), **kwargs)
+
 
 def check(host):
     if useWebHook:
         return fncs.check(host, webhook=DSICORD_WEBHOOK)
     else:
         return fncs.check(host)
+
 
 def scan(ip_list):
     try:
@@ -85,6 +88,7 @@ def scan(ip_list):
         Eprint(traceback.format_exc())
         return []
 
+
 def Eprint(text):
     """Error printer
 
@@ -92,8 +96,8 @@ def Eprint(text):
         text (String): Error text
     """
     text = str(text)
-    (disLog("Error: "+"".join(str(i) for i in text))) if useWebHook else None
-    fncs.dprint("\n"+"".join(str(i) for i in text)+"\n")
+    (disLog("Error: " + "".join(str(i) for i in text))) if useWebHook else None
+    fncs.dprint("\n" + "".join(str(i) for i in text) + "\n")
 
 
 def disLog(text, end="\r"):
@@ -105,22 +109,25 @@ def disLog(text, end="\r"):
             data = {"content": text + end}
             requests.post(url, data=data)
         except Exception:
-            Eprint(text+'\n'+traceback.format_exc())
+            Eprint(text + "\n" + traceback.format_exc())
+
 
 async def threader(ip_range):
     try:
         ips = scan(ip_range)
-        
+
         if len(ips) > 0:
-            pool = multiprocessing.pool.ThreadPool(maxActive//2)
+            pool = multiprocessing.pool.ThreadPool(maxActive // 2)
             pool.map(check, ips)
     except OSError:
         sys.exit(0)
     except Exception:
         Eprint(traceback.format_exc())
 
+
 def crank(ip_range):
     asyncio.run(threader(ip_range))
+
 
 # Main
 # ---------------------------------------------
@@ -137,23 +144,32 @@ time.sleep(0.5)
 
 tStart = time.time()
 normal = threading.active_count()
+
+
 async def makeThreads():
     # Create a thread for each list of IPs
     for ip_list in ip_lists:
         # check to make sure that more than 2*maxActive threads haven't been created in the last 1 second
-        if len(threads) >= maxActive*2 and time.time()-tStart <= 1:
+        if len(threads) >= maxActive * 2 and time.time() - tStart <= 1:
             await asyncio.sleep(0.5)
-            Eprint("Too many threads spawned to quickly, exiting\nPlease check the console and logs for more details")
+            Eprint(
+                "Too many threads spawned to quickly, exiting\nPlease check the console and logs for more details"
+            )
             sys.exit()
-        # Create the thread
-        t = threading.Thread(target=crank, args=(ip_list,),name=f"Scan func thread: {ip_list}")
-        # Add the thread to the list of threads
+
+        t = threading.Thread(
+            target=crank,
+            args=(ip_list,),
+            name=f"Scan func thread: {ip_list}"
+        )
+
         threads.append(t)
+
         # If the number of active threads is greater than the max, sleep for 0.1 seconds
-        while threading.active_count()-normal >= maxActive:
+        while threading.active_count() - normal >= maxActive:
             await asyncio.sleep(0.1)
         t.start()
 
 
-asyncio.run(makeThreads())
-
+if __name__ == "__main__":
+    asyncio.run(makeThreads())
